a893698b4c634d57e561d48da8750cd7a2f7bcce
loyl-script <majpotejto@gmail.com>
2026-03-03 14:09:55 +0300

    Ink. House

--- Архив ZIP - WinRAR/house/src/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-export const Button = (props) => {
-    return (
-        <button onClick={props.click} className={props.class}>{props.text}</button>
-    )
-}
--- Архив ZIP - WinRAR/house/src/components/Rights.jsx
+++
@@ -1,9 +0,0 @@
-const rights = () => {
-    const rights = 'Ink. House ® \nAll rights reserved'
-
-    return (
-        <div className="footerRights">{rights}</div>
-    )
-}
-
-export default rights
